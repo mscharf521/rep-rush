d106b3aa721d353ff3376be8f73adf821457812f
Matthew Scharf <mscharf521@gmail.com>
2025-02-18 10:17:20 -0500

    pwa2

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,22 @@
- 
+const CACHE_NAME = 'rep-rush-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+}); 
